acea4850c7c4af86abefbf04e883092bc631d486
Daniel Michnowicz <extern.daniel.michnowicz@vwfs.io>
2021-10-02 12:53:18 +0200

    css

--- src/components/TableComponent/TableComponent.tsx
+++ src/components/TableComponent/TableComponent.tsx
@@ -1,57 +1,52 @@
-import { Table } from "@fluentui/react-northstar"
-import * as React from "react"
+import {Table} from "@fluentui/react-northstar";
+import * as React from "react";
 import "./TableComponent.scss";
 
-
-const fiboHeader = {
-    items: [
-        <p className="headerText participants">Participants</p>,
-        <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/choice2.png" alt="choice2" className="element"/>,
-        <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/choice3.png" alt="choice3" className="element"/>,
-        <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/choice5.png" alt="choice5" className="element"/>,
-        <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/choice8.png" alt="choice8" className="element"/>,
-        <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/choice13.png" alt="choice13" className="element"/>,
-        <p className="headerText">Other cards</p>
-    ],
-  }
-
-  const tshirtsHeader = {
-    items: [
-        <p className="headerText participants">Participants</p>,
-        <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/shirtXS.png" alt="shirtXS" className="element"/>,
-        <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/shirtS.png" alt="shirtS" className="element"/>,
-        <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/shirtM.png" alt="shirtM" className="element"/>,
-        <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/shirtL.png" alt="shirtL" className="element"/>,
-        <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/shirtXL.png" alt="shirtXL" className="element"/>,
-        <p className="headerText">Other cards</p>
-    ],
-  }
-
-  const fiboItems = [
+const fiboItems = [
     <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/choice2.png" alt="choice2" className="element"/>,
     <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/choice3.png" alt="choice3" className="element"/>,
     <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/choice5.png" alt="choice5" className="element"/>,
     <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/choice8.png" alt="choice8" className="element"/>,
     <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/choice13.png" alt="choice13" className="element"/>
-  ]
+];
 
-  const tshirtItems = [
+const tshirtItems = [
     <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/shirtXS.png" alt="shirtXS" className="element"/>,
-        <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/shirtS.png" alt="shirtS" className="element"/>,
-        <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/shirtM.png" alt="shirtM" className="element"/>,
-        <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/shirtL.png" alt="shirtL" className="element"/>,
-        <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/shirtXL.png" alt="shirtXL" className="element"/>
-  ]
-  
-  export interface ITableComponentProps {
+    <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/shirtS.png" alt="shirtS" className="element"/>,
+    <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/shirtM.png" alt="shirtM" className="element"/>,
+    <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/shirtL.png" alt="shirtL" className="element"/>,
+    <img src="https://raw.githubusercontent.com/microsoft-teams-spoker/spoker/master-without-storybook/assets/images/custom/shirtXL.png" alt="shirtXL" className="element"/>
+];
+
+const fiboHeader = {
+    items: [
+        <p className="headerText participants">Participants</p>,
+        ...fiboItems,
+        <p className="headerText">Other cards</p>
+    ],
+};
+
+const tshirtsHeader = {
+    items: [
+        <p className="headerText participants">Participants</p>,
+        ...tshirtItems,
+        <p className="headerText">Other cards</p>
+    ],
+};
+
+function getDefaultCounterArray(): number[] {
+    return new Array(5).fill(0);
+}
+
+export interface ITableComponentProps {
     scale: string;
     allUsersPolls: ITableItem[];
     showShimmer?: boolean;
 }
 
 export interface ITableItem {
-    user: actionSDK.SubscriptionMember, 
-    responseIds: {[key:string]: string}
+    user: actionSDK.SubscriptionMember,
+    responseIds: { [key: string]: string }
 }
 
 /**
@@ -61,64 +56,40 @@ export class TableComponent extends React.PureComponent<ITableComponentProps> {
 
     render() {
         console.log("TABLE");
-        let header = fiboHeader;
-        if (this.props.scale !== "fibo") {
-            header = tshirtsHeader;
-        }
+        const header = this.props.scale === "fibo" ? fiboHeader : tshirtsHeader;
+        const defaultItems: JSX.Element[] = this.props.scale === "fibo" ? fiboItems : tshirtItems;
 
-        let counterArray: number[] = getDefaultCounterArray();
-        let rows: {key: number, items: JSX.Element[]}[] = [];
+        const counterArray: number[] = getDefaultCounterArray();
+        const rows: { key: number, items: JSX.Element[] }[] = [];
 
-        for (let index = 0; index < this.props.allUsersPolls.length; index++) {
+        this.props.allUsersPolls.forEach((user, index) => {
             const userPoll = this.props.allUsersPolls[index];
-            let defaultItems: JSX.Element[] = fiboItems;
-            if (this.props.scale !== "fibo") {
-                defaultItems = tshirtItems;
-            }
-
-            rows[index] = {
-                key: index,
-                items: [
-                    <p className="headerText">{userPoll.user.displayName}</p>,
-                    <p></p>,
-                    <p></p>,
-                    <p></p>,
-                    <p></p>,
-                    <p></p>,
-                    <p></p>
-                ]
-            };
-
             if (userPoll.responseIds) {
-                let place = parseInt(userPoll.responseIds['0']);
-                rows[index].items[place+1] = defaultItems[place];
+                rows[index] = {
+                    key: index,
+                    items: [
+                        <p className="headerText">{userPoll.user.displayName}</p>,
+                        ...fiboItems.map(() => <p></p>),
+                        <p></p>
+                    ]
+                };
+
+                const place = parseInt(userPoll.responseIds['0']);
+                rows[index].items[place + 1] = defaultItems[place];
                 counterArray[place] = counterArray[place] + 1;
             }
-        }
+        });
 
         rows[this.props.allUsersPolls.length] = {
             key: this.props.allUsersPolls.length,
             items: [
                 <p className="headerText">Results</p>,
-                <p className="headerText">{counterArray[0]}</p>,
-                <p className="headerText">{counterArray[1]}</p>,
-                <p className="headerText">{counterArray[2]}</p>,
-                <p className="headerText">{counterArray[3]}</p>,
-                <p className="headerText">{counterArray[4]}</p>,
+                ...counterArray.map((count) => <p className="headerText">{count}</p>),
                 <p className="headerText">{counterArray[5]}</p>,
             ]
-        }
+        };
         return (
-            <Table header={header} rows={rows} aria-label="Static table" />
+            <Table header={header} rows={rows} aria-label="Static table" className="votesTable"/>
         );
     }
-}
-
-function getDefaultCounterArray(): number[] {
-    let array = [];
-
-    for (let index = 0; index < 6; index++) {
-        array[index] = 0;
-    }
-    return array;
 }
